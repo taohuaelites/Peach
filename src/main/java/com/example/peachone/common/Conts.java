43a95d679c149e561d0c8677de741777f83c9bf4
hxwcafe <2691882550@qq.com>
2018-11-11 12:06:06 +0800

    sprintbooot+mybatis+jpa环境,授权接口

---
+++ src/main/java/com/example/peachone/common/Conts.java
@@ -0,0 +1,7 @@
+package com.example.peachone.common;
+
+public class Conts {
+    public static final String CURRENT_USER = "current_user";
+    public static final String USER_NAME = "username";// 用户名字段
+    public static final String OPENID = "openid";//用户标识
+}
