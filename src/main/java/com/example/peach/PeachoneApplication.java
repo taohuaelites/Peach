eb08bbecdc630a02b0eb811a9a9f588184f4c3b4
hxwcafe <2691882550@qq.com>
2018-11-11 14:34:06 +0800

    第三次提交

---
+++ src/main/java/com/example/peach/PeachoneApplication.java
@@ -0,0 +1,15 @@
+package com.example.peach;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.example.peach.mapper")
+public class PeachoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeachoneApplication.class, args);
+        System.out.println("hellow,word");
+    }
+}
---
+++ src/main/java/com/example/peach/service/UserService.java
@@ -0,0 +1,12 @@
+package com.example.peach.service;
+
+import com.example.peach.common.ServiceResponse;
+import com.example.peach.pojo.User;
+
+public interface UserService {
+//    ServiceResponse<User> selectUserByid(int id);
+
+    ServiceResponse<String> selectOpenid(String str,String type);
+
+    ServiceResponse<String> lognUser(User user);
+}
