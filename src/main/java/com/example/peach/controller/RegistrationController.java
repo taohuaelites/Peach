891910e9eb0af33d1b5d65db602444adf27c959a
765202455@qq.com <765202455@qq.com>
2018-11-11 16:57:44 +0800

    短信接口提交

---
+++ src/main/java/com/example/peach/controller/RegistrationController.java
@@ -0,0 +1,17 @@
+package com.example.peach.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 绑定手机号
+ * Created by Administrator on 2018/11/11.
+ */
+@RestController
+@RequestMapping(value="/Registration")
+public class RegistrationController {
+
+
+
+
+}
