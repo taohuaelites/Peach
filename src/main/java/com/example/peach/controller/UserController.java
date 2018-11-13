b578bdac68c4bacddf0dd37591fabc5b013931c2
765202455@qq.com <765202455@qq.com>
2018-11-13 15:14:28 +0800

    解决JPA

---
+++ src/main/java/com/example/peach/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.example.peach.controller;
+
+import com.example.peach.common.Conts;
+import com.example.peach.common.ServiceResponse;
+import com.example.peach.pojo.User;
+import com.example.peach.service.UserService;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/user")
+@MapperScan("com.example.peach.mapper")
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    /**
+     * 微信授权
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "/lognUser",method = RequestMethod.POST)
+    public ServiceResponse<String> lognUser(User user){
+        ServiceResponse<String> response = userService.lognUser(user);
+        System.out.println();
+        return response;
+    }
+
+    /**
+     * 查询用户唯一标识
+     * @param openid
+     * @return
+     */
+    @RequestMapping(value = "/findUserByOpenid",method = RequestMethod.POST)
+    public ServiceResponse<String> selectOpenid(String openid){
+        ServiceResponse<String> response = userService.selectOpenid(openid, Conts.OPENID);
+        return response;
+    }
+
+    /*@RequestMapping(value = "/registerUser",method = RequestMethod.POST)
+    public  ServiceResponse<String> re*/
+}
---
+++ src/main/java/com/example/peach/service/impl/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.peach.service.impl;
+
+import com.example.peach.common.Conts;
+import com.example.peach.common.ServiceResponse;
+import com.example.peach.dao.UserJPA;
+import com.example.peach.dao.UserMapper;
+import com.example.peach.pojo.User;
+import com.example.peach.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Resource
+    private UserMapper userMapper;
+    @Resource
+    private UserJPA userJPA;
+    //查询标识
+    @Override
+    public ServiceResponse<String> selectOpenid(String str, String type) {
+        User user = userJPA.findByOpenid(str);
+        if(type.equals(Conts.OPENID)){
+            if(user ==null){
+                return  ServiceResponse.createBySuccess("用户需要注册");
+
+            }
+        }
+        return ServiceResponse.createByError("用户已经注册过");
+    }
+
+    //录入授权信息
+    @Override
+    public ServiceResponse<String> lognUser(User user) {
+        ServiceResponse<String> response =  this.selectOpenid(user.getOpenid(),Conts.OPENID);
+        if(!response.isSuccess()){
+            return response;
+        }
+        User userlogn = userJPA.save(user);
+        if(userlogn==null){
+            return  ServiceResponse.createByError("授权失败!!");
+        }
+        return ServiceResponse.createBySuccess("授权成功");
+    }
+
+}
